d68882e2d9f26417ef16e4bc85c908887e0c7f85
xinmu <1670417475@qq.com>
2026-03-18 14:08:48 +0800

    【新增功能】菜品分页查询

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,7 +1,9 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.mapper.DishMapper;
+import com.sky.result.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public interface DishService {
@@ -11,4 +13,11 @@ public interface DishService {
      * @param dishDTO
      */
     public void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
