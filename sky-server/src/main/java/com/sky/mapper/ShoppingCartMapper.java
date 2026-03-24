20e666157e9aeb223125e5be017e49375ebd1204
xinmu <1670417475@qq.com>
2026-03-24 19:25:09 +0800

    【新增功能】清空购物车

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -1,7 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.ShoppingCart;
-import com.sky.service.ShoppingCartService;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
@@ -24,7 +24,18 @@ public interface ShoppingCartMapper {
     @Update("update shopping_cart set number = ${number} where id = ${id}")
     void updateNumberById(ShoppingCart shoppingCart);
 
+    /**
+     * 插入购物车数据
+     * @param shoppingCart
+     */
     @Insert("insert into shopping_cart( name, image, user_id, dish_id, setmeal_id, dish_flavor, number, amount, create_time)" +
             "values(#{name} , #{image} , #{userId} , #{dishId} , #{setmealId} , #{dishFlavor} , #{number} , #{amount} , #{createTime})")
     void insert(ShoppingCart shoppingCart);
+
+    /**
+     * 根据用户id删除购物车数据
+     * @param userId
+     */
+    @Delete("delete from shopping_cart where user_id = ${userId}")
+    void deleteByUserId(Long userId);
 }
