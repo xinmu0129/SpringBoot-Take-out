2f514c7e03b26ccca875959eb1760bb0af1ec9bc
xinmu <1670417475@qq.com>
2026-03-24 19:07:56 +0800

    【新增功能】查看购物车

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -78,4 +78,17 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
 
     }
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    public List<ShoppingCart> showShoppingCart(){
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(userId)
+                .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
+    }
 }
