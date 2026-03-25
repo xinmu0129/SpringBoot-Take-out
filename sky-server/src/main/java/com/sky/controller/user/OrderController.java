c2ca15f3121af2cf6b2c53fd932ce291e272949b
xinmu <1670417475@qq.com>
2026-03-25 14:59:21 +0800

    【新增功能】用户下单

---
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,37 @@
+package com.sky.controller.user;
+
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderSubmitVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("userOrderController")
+@RequestMapping("/user/order")
+@Slf4j
+@Api(tags = "C端订单接口")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    /**
+     * 用户下单
+     * @param ordersSubmitDTO
+     * @return
+     */
+    @RequestMapping("submit")
+    @ApiOperation("用户下单")
+    public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO ordersSubmitDTO){
+        log.info("用户下单：{}", ordersSubmitDTO);
+        OrderSubmitVO orderSubmitVO = orderService.submitOrder(ordersSubmitDTO);
+        return Result.success(orderSubmitVO);
+    }
+}
